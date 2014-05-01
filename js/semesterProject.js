8bda2f949790ec3c0e322b99667cde6c8ee09f30
Frank FitzGerald <me@frankfitzgerald.net>
2014-05-01 16:53:59 -0400

    Update semesterProject.js

--- js/semesterProject.js
+++ js/semesterProject.js
@@ -22,11 +22,11 @@ $(document).ready(function() {
 	  var html = ['<ul>'];
 	  for (var i = 0; i < entries.length; i++) {
 	    var entry = entries[i].content.src;
-	    var content = '<object width="560" height="315"><param name="movie" value='+entry+'></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/wbB3lVyUvAM?hl=en_US&amp;version=3" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
+	    consolde.log(entry);
+	    var content = '<object width="560" height="315"><param name="movie" value='+entry+'></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/wbB3lVyUvAM?hl=en_US&amp;version=3" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true"></embed></object>',
 	    html.push('<li>', content, '</li>');
 	  }
 	  html.push('</ul>');
-	  console.log($("#responseDiv").html());
 	  $("#responseDiv").html(html.join(''));
 	} 
     $('form').bind('submit',function(e) {
