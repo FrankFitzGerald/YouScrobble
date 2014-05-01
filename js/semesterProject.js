498c771df9d1257c4fb1e9fba67ca6b50102fdd2
Frank FitzGerald <me@frankfitzgerald.net>
2014-05-01 16:44:40 -0400

    Update semesterProject.js

--- js/semesterProject.js
+++ js/semesterProject.js
@@ -18,12 +18,12 @@ $(document).ready(function() {
 	// 	html.push('</ul>');
 	// }
 	function show_my_videos(data) {
-	  var feed = data.feed;
-	  var entries = feed.entry;
+	  var entries = data.feed.entry;
 	  var html = ['<ul>'];
 	  for (var i = 0; i < entries.length; i++) {
-	    var entry = entries[i].content.src;
-	    var content = '<object width="560" height="315"><param name="movie" value=entry></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/wbB3lVyUvAM?hl=en_US&amp;version=3" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
+	    var entry = entries[i];
+	    var video = entry.content.src
+	    var content = '<object width="560" height="315"><param name="movie" value='+video+'></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/wbB3lVyUvAM?hl=en_US&amp;version=3" type="application/x-shockwave-flash" width="560" height="315" allowscriptaccess="always" allowfullscreen="true"></embed></object>';
 	    html.push('<li>', content, '</li>');
 	  }
 	  html.push('</ul>');
